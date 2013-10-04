fc837db08c0210cc45eaa2f2ee1b2cde48d60917
Kevin Hodges <kevin.hodges@holidayextras.com>
2013-10-04 16:58:32 +0100

    commit

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
+/**
+* @name /test.js
+* @description Nodejs style test include
+* @since Fri Oct 04 16:47:15 BST 2013
+* @author Kevin Hodges <kevin.hodges@holidayextras.com>
+*/
 
 module.exports = require( './test/sanity' );
